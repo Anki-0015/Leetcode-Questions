059c637e97170b5936bdcd83cc2eee1f0564d07c
Ankit <156998935+Anki-0015@users.noreply.github.com>
2024-12-12 22:00:48 +0530

    Create Take Gifts From the Richest Pile.java
    problem 2558

---
+++ Take Gifts From the Richest Pile.java
@@ -0,0 +1,17 @@
+class Solution {
+    
+    public long pickGifts(int[] gifts, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
+        for(int i:gifts){
+            pq.add(i);
+        }
+        while(k-->0){
+            pq.add((int)Math.sqrt(pq.poll()));
+        }
+        long sum=0;
+        while(pq.size()!=0){
+            sum=sum+pq.poll();
+        }
+        return sum;
+    }
+}
